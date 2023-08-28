efb5cbdbe243547c61fe0ac96bdb04edf5fa4bef
Dan Morton <dan.morton@fullscript.com>
2023-08-28 15:05:05 -0400

    hello world

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const http = require("http");
 
-const app = express();
-const PORT = 3000; // You can change this to any available port you prefer
-
-// Middleware to parse JSON data
-app.use(bodyParser.json());
-
-// Storage for received JSON data
-const jsonData = [];
-
-// Endpoint to store JSON data
-app.post("/data", (req, res) => {
-  const data = req.body;
-  jsonData.push(data);
-  res.status(201).json({ message: "JSON data stored successfully" });
+// Create a simple HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
 });
 
-// Start the server
-app.listen(PORT, () => {
+// Define the port on which the server will listen
+const PORT = 3000;
+
+// Start the server and listen on the defined port
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
